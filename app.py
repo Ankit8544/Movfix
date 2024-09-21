37e333d8b188a180e98bad8e6a85d79d6a705849
Ankit8544 <ankitkumar875740@gmail.com>
2024-09-22 01:27:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, render_template
+import json
+import requests
+import pickle
+import os
+import io
+import gdown
+import random
+
+app = Flask(__name__)
+
+# Download the file
+url = 'https://drive.google.com/uc?export=download&id=1bzmDYhHCOCI0dRLF72-6rt3DstLGbXT7'
+response = requests.get(url)
+
+# Load the pickle file from the response content
+if response.status_code == 200:
+    movie_list = pickle.load(io.BytesIO(response.content))
+else:
+    print(f"Error downloading file: {response.status_code}")
+    
+# Download the file
+url = 'https://drive.google.com/uc?id=1xrVHcbqtvdX5J435kvJ1FkQP8bT_Hj0e'
+
+output = 'similarity.pkl'  # Temporary file to save the downloaded data
+gdown.download(url, output, quiet=False)
+
+# Load the pickle file
+with open(output, 'rb') as f:
+    similarity = pickle.load(f)
+
+# Clean up if needed (remove the temporary file)
+os.remove(output)
+
+# Function to get movie recommendations
+def recommend(movie):
+    try:
+        index = movie_list[movie_list['title'] == movie].index[0]
+        distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+        recommended_movies = [movie_list.iloc[i[0]].title for i in distances[1:6]]
+        return recommended_movies
+    except IndexError:
+        return ["Movie not found in the database."]
+
+# Function to fetch movie details from the OMDB API
+def movie_detail(movie_name):
+    url = f"http://www.omdbapi.com/?t={movie_name}&apikey=7058f3e1"
+    response = requests.get(url)
+    movie_data = json.loads(response.text)
+    return movie_data
+
+@app.route('/')
+def home():
+    movie_titles = movie_list['title'].to_list()
+    random_movies = random.sample(movie_titles, 4)
+
+    home_movies = []
+
+    for i in random_movies:
+        movie = {
+            'title': movie_detail(i)['Title'],
+            'year': movie_detail(i)['Year'],
+            'runtime': movie_detail(i)['Runtime'],
+            'imdbrating': movie_detail(i)['imdbRating'],
+            'poster': movie_detail(i)['Poster']
+        }
+        home_movies.append(movie)
+
+    return render_template('index.html', home_movies=home_movies)
+
+# Route to display the home page with movie details
+@app.route('/aaaa')
+def aaaa():
+    movie_name = request.args.get('movie_name', 'Avatar')
+    # Get recommended movies list
+    movie_suggestions_list = recommend(movie_name)
+    movie_suggestions_list = movie_suggestions_list[:-1]
+    movie_data = movie_detail(movie_name)
+    movies_suggestions_data = []
+
+    # Fetch details for each recommended movie
+    for i in movie_suggestions_list:
+        suggested_movie = movie_detail(i)
+        movie = {
+            'title': suggested_movie['Title'],
+            'year': suggested_movie['Year'],
+            'runtime': suggested_movie['Runtime'],
+            'imdbrating': suggested_movie['imdbRating'],
+            'poster': suggested_movie['Poster']
+        }
+        movies_suggestions_data.append(movie)
+
+    return render_template('movie-details.html', movie_data=movie_data, suggested_movies=movies_suggestions_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)

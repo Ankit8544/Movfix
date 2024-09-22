853e73f6678393b0a3b4c2548b8d710df2a2fd61
Ankit8544 <ankitkumar875740@gmail.com>
2024-09-22 18:37:44 +0530

    Update

--- app.py
+++ app.py
@@ -112,5 +112,4 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Fallback to 10000 if PORT is not set
-    app.run(host='0.0.0.0', port=port)
+    app.run()
